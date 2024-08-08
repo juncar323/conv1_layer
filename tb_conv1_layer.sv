0246207cdf2869e389e38639f9ddb3ea13d79a02
JUN___ <juncar323@gmail.com>
2024-08-08 16:51:15 +0900

    Update tb_conv1_layer.sv

--- tb_conv1_layer.sv
+++ tb_conv1_layer.sv
@@ -6,7 +6,7 @@ reg rst_n;
 reg [31:0] data;
 reg valid_in;
 wire valid_out;
-wire [31:0] conv1_out[0:8];
+wire [31:0] conv1_out[0:31];
 conv1_layer conv1_layer (
    .clk(clk),
    .rst_n(rst_n),
@@ -22,18 +22,26 @@ clk = 0;
 forever #5 clk = ~clk;
 end
 
+
 initial begin
 valid_in = 0 ;
 rst_n = 1;
 #2 rst_n = 0;
 #2 rst_n = 1;
 #1 valid_in = 1;
-data = $random;
-for (int i = 0; i<28*36;i=i+1) begin
-#10 data = $random;
+data = 0;
+for (int i = 0; i<27;i=i+1) begin
+#10 data = 0;
 end
+for (int i = 0; i<28*36-56;i=i+1) begin
+#10 data = 0;
+end
+for (int i = 0; i<28;i=i+1) begin
+#10 data = 0;
+end
+#10 data = 32'bx;
 valid_in = 0;
-#5000
+#20000
 $finish;
 end
 
